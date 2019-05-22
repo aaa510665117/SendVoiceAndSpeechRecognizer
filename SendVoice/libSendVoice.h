26c4246ca7d373ffc3541746ee80426c9cc64582
扬张 <510665117@qq.com>
2019-05-22 16:00:22 +0800

    静态库

---
+++ SendVoice/libSendVoice.h
@@ -0,0 +1,15 @@
+//
+//  libSendVoice.h
+//  libSendVoice
+//
+//  Created by ZY on 2019/5/22.
+//  Copyright © 2019 扬张. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SOSAudioVoice.h"
+#import "SpeechRecognizerManager.h"
+
+@interface libSendVoice : NSObject
+
+@end
